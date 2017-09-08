61c35669e0136273503b624c589c1c6c40202095
vijay5m4 <31735026+vijay5m4@users.noreply.github.com>
2017-09-08 16:36:21 +0530

    stu

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
+		StudentGroup s=new Student(5);
+		a.add();
+		s.getStudents();
+		s.setStudents();
+		s.getStudentsByAge();
+		s.getStudentsWithAvgMark();
 	}
 
 }
